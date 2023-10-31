6645f85fc28022fe2ef7fd4d8f057b6807ddd157
KAZUAKI KADOYA <kadoya@penpale.jp>
2023-10-31 12:01:34 +0900

    Correct the drag&drop area style

--- src/lib/metaViewer.svelte
+++ src/lib/metaViewer.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	// import fileIcon from "../assets/file-solid.svg";
+
 	let files;
 	let results = [];
 	let showImage = false; //Image flag
@@ -28,6 +30,7 @@
 							"(.*?)$";
 						const regex = new RegExp(regexPattern, "gs");
 						let matches = regex.exec(tempMetaData);
+						console.log(matches);
 
 						const metaInfo = {
 							prompt: matches[1],
@@ -109,7 +112,6 @@
 
 			offset += 12 + chunkLength;
 		}
-
 		return chunks;
 	}
 
@@ -125,7 +127,8 @@
 
 <div class="file_dropper">
 	<label for="dropper">
-		Drop PNGs here.
+		<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512" class="fileicon"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z"/></svg><br>
+		<div>Drop Generated PNG images here.</div>
 		<input
 			bind:files
 			on:change={handleFilesChange}
@@ -135,6 +138,7 @@
 			accept="image/png"
 		/>
 	</label>
+	
 </div>
 
 <div class="ctrl">
